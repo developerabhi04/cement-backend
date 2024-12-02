683d950d0398cdd446778c4be0493b514b3ea969
stark <developerabhi04@gmail.com>
2024-12-02 18:38:18 +0530

    hello

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(errorMiddleware)
 
 
 app.listen(PORT, (req, res) => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
 
 
